b66604c7217afb1bc88e89b0a6cdd265701aea7c
LeonidMelnichenko <122801542+LeonidMelnichenko@users.noreply.github.com>
2024-03-07 09:51:47 +0300

    create TestNumber for Task 1

---
+++ src/Task_1/TestNumber.java
@@ -0,0 +1,13 @@
+package Task_1;
+
+public class TestNumber {
+    public static void testMethod(Double number) {
+        if (number != null) {
+            if (number > 7) {
+                System.out.println("Hello");
+            } else {
+                System.out.println("Enter other number");
+            }
+        }
+    }
+}
