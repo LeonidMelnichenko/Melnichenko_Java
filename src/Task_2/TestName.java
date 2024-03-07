09625546f375a9463048b53863c27644893bbd00
LeonidMelnichenko <122801542+LeonidMelnichenko@users.noreply.github.com>
2024-03-07 09:52:07 +0300

    create TestName for Task 2

---
+++ src/Task_2/TestName.java
@@ -0,0 +1,11 @@
+package Task_2;
+
+public class TestName {
+    public static void testMethod(String name) {
+        if (name.equalsIgnoreCase("Вячеслав")) {
+            System.out.println("Привет, Славик");
+        } else {
+            System.out.println("Нет такого имени");
+        }
+    }
+}
